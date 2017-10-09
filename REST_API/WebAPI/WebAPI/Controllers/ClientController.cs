83ecd2e5f64c9b64c9a387b4a04e1933b3f2ce8a
efren <carva97valvi@gmail.com>
2017-10-08 22:09:55 -0600

    Version 1 del rest, agregado controlado cliente

--- REST_API/WebAPI/WebAPI/Controllers/ClientController.cs
+++ REST_API/WebAPI/WebAPI/Controllers/ClientController.cs
@@ -4,16 +4,38 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using System.Web.Http.Cors;
+using Newtonsoft.Json.Linq;
+using System.Web.Http.Results;
+using System.Web.Mvc;
 
 namespace WebAPI.Controllers
 {
+    /**
+    * Client Controller
+    * */
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
     public class ClientController : ApiController
     {
-        public string[] Get() {
-            return new string[] {
-                "Hello",
-                "World"
-            };
+        public string Get(string x) {
+            JObject response =
+                new JObject(
+                new JProperty("Code", 200),
+                new JProperty("Description", "Succesfull")
+                );
+            return x;
+        }
+
+        public JObject Post(JObject x) {
+            dynamic temp = x;
+            int temp2 = (int)temp.id;
+            JObject response =
+               new JObject(
+               new JProperty("Code", 100),
+               new JProperty("Description", "Succesfull"),
+               new JProperty("Id",temp2)
+               );
+            return response;
         }
     }
 }
--- REST_API/WebAPI/WebAPI/Controllers/HomeController.cs
+++ REST_API/WebAPI/WebAPI/Controllers/HomeController.cs
@@ -4,13 +4,13 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
-namespace WebAPI.Controllers
+namespace WebApplication1.Controllers
 {
     public class HomeController : Controller
     {
         public ActionResult Index()
         {
-            ViewBag.Title = "Home Page";
+            ViewBag.Title = "Login Page";
 
             return View();
         }
